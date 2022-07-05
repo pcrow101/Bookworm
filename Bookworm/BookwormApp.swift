6a83f9329438de3c3fa292ea925f01a88847f9e0
Paul Crow <paucrow@synamedia.com>
2022-07-05 09:18:53 +0100

    @Bindings

---
+++ Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by paucrow on 05/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
